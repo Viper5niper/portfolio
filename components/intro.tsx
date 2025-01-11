bac0ebaae50e4c313d39818cfc2d5bb90aa3f7d9
Ruben Mulato <43819949+Viper5niper@users.noreply.github.com>
2025-01-11 14:24:46 -0600

    fix: leetcode profile link

--- components/intro.tsx
+++ components/intro.tsx
@@ -117,7 +117,7 @@ export default function Intro() {
         </a>
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://leetcode.com/u/rubenmulato99/r"
+          href="https://leetcode.com/u/rubenmulato99"
           target="_blank"
         >
           <SiLeetcode />
